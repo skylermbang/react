1959fdc87e2ed984738207b4132f0d009fae3542
Skyler Bang <bang618@hotmail.com>
2024-06-21 16:19:43 +1000

    feat add new screen

---
+++ coin-tracker/src/Router.tsx
@@ -0,0 +1,20 @@
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Coin from "./routes/Coin";
+import Coins from "./routes/Coins";
+
+function Router() {
+  return (
+    <BrowserRouter>
+      <Switch>
+        {/* //this is how you tell there is parameter */}
+        <Route path="/:coinId">
+          <Coin />
+        </Route>
+        <Route path="/">
+          <Coins />
+        </Route>
+      </Switch>
+    </BrowserRouter>
+  );
+}
+export default Router;
---
+++ coin-tracker/src/routes/Coins.tsx
@@ -0,0 +1,94 @@
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import styled from "styled-components"
+
+const Title = styled.h1`
+    color:${props=>props.theme.accentColor};
+    font-size: 48px;
+`;
+
+const Container = styled.div`
+    padding: 0px 20px;
+    max-width:480px;
+    margin: 0 auto;
+`
+
+const Header = styled.div`
+    height:10vh;
+    display:flex;
+    justify-content: center;
+    align-items: center;
+    
+`
+
+const CoinsList = styled.ul``
+
+const Coin = styled.li`
+    background-color:white;
+    color:${(props)=> props.theme.bgColor};
+    margin-bottom: 10px;
+    border-radius: 15px;
+    a{
+        transition: color 0.25s ease-in;
+        display:block;
+        padding:20px;
+    }
+    &:hover{
+        a{
+            color:${(props)=>props.theme.accentColor}
+        }
+    }
+`
+
+const Loading = styled.div`
+    height:10vh;
+    display:flex;
+    justify-content: center;
+    align-items: center;
+    text-align:center;
+`
+
+interface CoinInterface{
+    id: string,
+    name: string,
+    symbol: string,
+    rank: number,
+    is_new: boolean,
+    is_active: boolean,
+    type: string,
+}
+
+
+
+
+
+function Coins(){
+    const [coins, setCoins]= useState<CoinInterface[]>([]);
+    const [loading, setLoading]= useState(true);
+    useEffect(()=>{
+        (async()=>{
+         const response= await fetch("https://api.coinpaprika.com/v1/coins")
+         const json= await response.json()
+         //axios will return in json but fetch will have to convert to json
+         setCoins(json.slice(0,99))
+         setLoading(false)
+        })();
+    }   ,[])
+
+    return (
+        <Container>
+            <Header>
+                <Title>  Coin </Title>
+            </Header>    
+            {loading? <Loading> Loading the Coin Data ... </Loading>: <CoinsList>
+                {coins.map((coin)=>(
+                    <Coin key={coin.id}> 
+                        <Link to={`/${coin.id}`}>
+                    {coin.name} &rarr;</Link></Coin>
+                ))}
+            </CoinsList>}
+        </Container>
+    )
+}
+
+export default Coins
